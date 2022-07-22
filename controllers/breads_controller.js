0a326a286d8558780df10144cf0a3933dcab2292
Smith <william.smith@bankerslife.com>
2022-07-22 19:31:50 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,9 +4,13 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send('This is the index at /breads')
+  res.render('index', 
+   {
+    breads: Bread
+   }
+  )
+//res.send (Bread)
 })
-
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
---
+++ views/index.jsx
@@ -0,0 +1,27 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads}) {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        {/*This is a JSX comment.*/}
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        <ul>
+            {
+                breads.map((bread, index)=> {
+                    return (
+                     <li key={index}>
+                      <a href={`/breads/${index}`}>
+                        {bread.name}
+                      </a>
+                    </li>
+                    )
+                })
+            }
+        </ul>
+      </Default>
+    )
+}
+
+module.exports = Index
