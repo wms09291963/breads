e042857c598a6c0ad614a2cb28dfae62eb8b1ed9
Smith <william.smith@bankerslife.com>
2022-07-26 08:53:10 -0400

    create and new routes complete

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+
 // MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 // ROUTES
