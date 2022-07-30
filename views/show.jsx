a8cbce2e34898bf0be9dec178a02667f36edcd20
Smith <william.smith@bankerslife.com>
2022-07-30 05:30:26 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -19,6 +19,8 @@ function Show ({bread, index}) {
           </p>
           <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
+          <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
 </form>
           <img src={bread.image} alt={bread.name} />
           <li><a href="/breads">Go Home</a></li>
